92b4d2d33e0e310950f96754831574f6841c1117
Safijah <75682588+Safijah@users.noreply.github.com>
2021-03-22 15:21:17 +0100

    Pozivnica, Sala i Fotografi

---
+++ RS_SEMINARSKI/RS_SEMINARSKI/Controllers/FotografController.cs
@@ -0,0 +1,149 @@
+﻿using Data.EF;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RS_SEMINARSKI.ViewModels;
+using Data.EFModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace RS_SEMINARSKI.Controllers
+{
+    public class FotografController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment WebHostEnvironment;
+
+        public FotografController(ApplicationDbContext dbContext, ILogger<HomeController> logger,
+            IWebHostEnvironment webhostEnvironment)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+            WebHostEnvironment = webhostEnvironment;
+        }
+        public IActionResult PrikazFotografa(int KorisnikID)
+        {
+
+            
+            List<FotografPrikazVM.Row> fotograf = _dbContext.Fotografi.Select(d => new FotografPrikazVM.Row
+            {
+                FotografID = d.FotografID,
+                SatnicaSlikanja = d.SatnicaSlikanja,
+                Fotografija = d.Fotografija.NazivStilaFotografije,
+                PutanjaDoSlikeFotografa = d.PutanjaDoSlikeFotografa,
+                ImeFotografa = d.ImeFotografa,
+                PrezimeFotografa=d.PrezimeFotografa
+
+
+            }).ToList();
+
+            Korisnik temp = _dbContext.Korisnici.Find(KorisnikID);
+            FotografPrikazVM d = new FotografPrikazVM();
+             d.fotografi= fotograf;
+            d.KorisnikID = KorisnikID;
+            if (temp.RolaID == 1)
+            {
+                d.RolaID = 1;
+            }
+            else
+            {
+                d.RolaID = 2;
+            }
+
+
+            return View("PrikazFotografa", d);
+        }
+        public IActionResult EvidentirajFotografa(int KorisnikID, int FotografID = 0)
+        {
+           
+            List<SelectListItem> vrstefotografije = _dbContext.Fotografije.Select(
+                c => new SelectListItem
+                {
+                    Value = c.FotografijaID.ToString(),
+                    Text = c.NazivStilaFotografije
+
+                }).ToList();
+            FotografEvidentirajVM fotograf = new FotografEvidentirajVM();
+            fotograf.KorisnikID = KorisnikID;
+            if (FotografID == 0)
+            {
+                fotograf = new FotografEvidentirajVM();
+            }
+            else
+            {
+                fotograf = _dbContext.Fotografi
+                    .Where(s => s.FotografID == FotografID)
+                    .Select(c => new FotografEvidentirajVM
+                    {
+                        FotografID = c.FotografID,
+                        SatnicaSlikanja = c.SatnicaSlikanja,
+                        PutanjaDoSlikeFotografa = c.PutanjaDoSlikeFotografa,
+                        ImeFotografa = c.ImeFotografa,
+                        PrezimeFotografa=c.PrezimeFotografa,
+                        
+
+
+                    }).SingleOrDefault();
+            }
+            fotograf.KorisnikID = KorisnikID;
+            fotograf.FotografID = FotografID;
+           fotograf.Fotografija = vrstefotografije;
+            return View(fotograf);
+        }
+        public IActionResult Snimi(FotografEvidentirajVM x)
+        {
+           
+            Fotograf fotograf = new Fotograf();
+            x.PutanjaDoSlikeFotografa = UploadFile(x);
+            if (x.FotografID == 0)
+            {
+                _dbContext.Add(fotograf);
+            }
+            else
+            {
+                fotograf = _dbContext.Fotografi.Find(x.FotografID);
+            }
+
+            fotograf.FotografID = x.FotografID;
+            fotograf.SatnicaSlikanja = x.SatnicaSlikanja;
+            if (!string.IsNullOrEmpty(x.PutanjaDoSlikeFotografa))
+                fotograf.PutanjaDoSlikeFotografa = x.PutanjaDoSlikeFotografa;
+            fotograf.FotografijaID = x.FotografijaID;
+            fotograf.ImeFotografa = x.ImeFotografa;
+            fotograf.PrezimeFotografa = x.PrezimeFotografa;
+            _dbContext.SaveChanges();
+            return Redirect("PrikazFotografa?KorisnikID=" + x.KorisnikID);
+        }
+        
+        private string UploadFile(FotografEvidentirajVM x)
+        {
+            string fileName = null;
+            if  (x.SlikaFotografa!= null)
+            {
+                string uploadDir = Path.Combine(WebHostEnvironment.WebRootPath, "Slike");
+                fileName = Guid.NewGuid().ToString() + "-" + x.SlikaFotografa.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    x.SlikaFotografa.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+        public IActionResult ObrisiFotografa(int KorisnikID, int FotografID)
+        {
+
+           
+
+            Fotograf pronadjen = _dbContext.Fotografi.Find(FotografID);
+            _dbContext.Remove(pronadjen);
+            _dbContext.SaveChanges();
+            return Redirect("PrikazFotografa?KorisnikID=" + KorisnikID);
+        }
+    }
+}
---
+++ RS_SEMINARSKI/RS_SEMINARSKI/Controllers/PozivnicaController.cs
@@ -0,0 +1,137 @@
+﻿using Data.EF;
+using Data.EFModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RS_SEMINARSKI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RS_SEMINARSKI.Controllers
+{
+    public class PozivnicaController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment WebHostEnvironment;
+
+        public PozivnicaController(ApplicationDbContext dbContext, ILogger<HomeController> logger,
+            IWebHostEnvironment webhostEnvironment)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+            WebHostEnvironment = webhostEnvironment;
+        }
+        public IActionResult PrikazPozivnica(int KorisnikID)
+        {
+            
+            List<PozivnicaPrikazVM.Row> pozivnice = _dbContext.Pozivnice
+                .Select(x => new PozivnicaPrikazVM.Row
+                {
+                    PozivnicaID = x.PozivnicaID,
+                    CijenaPozivnice = x.CijenaPozivnice,
+                    OpisPozivnice = x.OpisPozivnice,
+                    PutanjaDoSlikePozivnice = x.PutanjaDoSlikePozivnice
+                }).ToList();
+            Korisnik temp = _dbContext.Korisnici.Find(KorisnikID);
+            PozivnicaPrikazVM x = new PozivnicaPrikazVM();
+            if (temp.RolaID == 1)
+                x.RolaID = 1;
+            else
+                x.RolaID = 2;
+            x.pozivnice = pozivnice;
+            x.KorisnikID = KorisnikID;
+            return View(x);
+        }
+
+        public IActionResult EvidentirajPozivnicu(int KorisnikID, int PozivnicaID = 0)
+        {
+           
+
+            PozivnicaEvidentirajVM pozivnica = new PozivnicaEvidentirajVM();
+            pozivnica.KorisnikID = KorisnikID;
+            if (PozivnicaID == 0)
+            {
+                pozivnica = new PozivnicaEvidentirajVM();
+            }
+            else
+            {
+                pozivnica = _dbContext.Pozivnice
+                    .Where(s => s.PozivnicaID == PozivnicaID)
+                    .Select(c => new PozivnicaEvidentirajVM
+                    {
+                        PozivnicaID = c.PozivnicaID,
+                        OpisPozivnice = c.OpisPozivnice,
+                        CijenaPozivnice= c.CijenaPozivnice,
+                        PutanjaDoSlikePozivnice = c.PutanjaDoSlikePozivnice
+                        
+
+                    }).SingleOrDefault();
+            }
+            pozivnica.KorisnikID = KorisnikID;
+
+            pozivnica.PozivnicaID = PozivnicaID;
+            return View(pozivnica);
+        }
+        public IActionResult Snimi(PozivnicaEvidentirajVM x)
+        {
+          
+            Pozivnica pozivnica = new Pozivnica();
+            x.PutanjaDoSlikePozivnice = UploadFile(x);
+            if (x.PozivnicaID == 0)
+            {
+                _dbContext.Add(pozivnica);
+            }
+            else
+            {
+                pozivnica = _dbContext.Pozivnice.Find(x.PozivnicaID);
+            }
+            pozivnica.PozivnicaID = x.PozivnicaID;
+            pozivnica.OpisPozivnice = x.OpisPozivnice;
+            pozivnica.CijenaPozivnice = x.CijenaPozivnice;
+            if (!string.IsNullOrEmpty(x.PutanjaDoSlikePozivnice))
+                pozivnica.PutanjaDoSlikePozivnice = x.PutanjaDoSlikePozivnice;
+            _dbContext.SaveChanges();
+            return Redirect("PrikazPozivnica?KorisnikID=" + x.KorisnikID);
+        }
+       
+
+
+        private string UploadFile(PozivnicaEvidentirajVM x)
+        {
+            string fileName = null;
+            if (x.SlikaPozivnice != null)
+            {
+                string uploadDir = Path.Combine(WebHostEnvironment.WebRootPath, "Slike");
+                fileName = Guid.NewGuid().ToString() + "-" + x.SlikaPozivnice.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    x.SlikaPozivnice.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
+        public IActionResult ObrisiPozivnicu(int KorisnikID, int PozivnicaID)
+        {
+
+           
+
+            Pozivnica pronadjen = _dbContext.Pozivnice.Find(PozivnicaID);
+
+
+
+
+            _dbContext.Remove(pronadjen);
+            _dbContext.SaveChanges();
+
+
+
+
+            return Redirect("PrikazPozivnica?KorisnikID=" + KorisnikID);
+        }
+    }
+}
